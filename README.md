4fef8f626956456fa6e3a67f8b840868e3648838
aleks-123 <ace.ki@hotmail.com>
2023-09-26 23:02:20 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# graph-appolo-test
---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+require("dotenv").config();
+const { graphqlHTTP } = require("express-graphql");
+const colors = require("colors");
+const schema = require("./schema/schema"); // No curly braces
+const port = process.env.PORT || 5000;
+const connectDB = require("./config/db");
+const app = express();
+
+// Connect to Database
+connectDB();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
+
+app.listen(port, console.log(`Server running on port ${port}`));
